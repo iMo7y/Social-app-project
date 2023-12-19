b474f54ed3dde73c46ce764d0d3dbf17c3a41071
Mohammed <saadisaadi768@gmail.com>
2023-12-19 00:14:44 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello MoeLink
+    </h1>
+  )
+}
+
+export default App
