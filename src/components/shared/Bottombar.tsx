1fc9832b184473f44ad5c5c4fa8f25682693f91c
Mohammed <saadisaadi768@gmail.com>
2023-12-29 23:01:05 -0600

    worked on bottom bar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { bottombarLinks } from '@/constants';
+import { Link, useLocation } from 'react-router-dom'
 
 const Bottombar = () => {
+  const { pathname } = useLocation()
+
   return (
-    <div>Bottombar</div>
+    <section className='bottom-bar'>
+    {bottombarLinks.map((link) => {
+            const isActive = pathname === link.route;
+
+            return (
+                <Link
+                to={link.route}
+                key={link.label} 
+                className={`${isActive && 'bg-5cbdea rounded-[10px]'} flex-center flex-col gap-1 p-2 transition text-light-1 `}>
+                <img 
+                src={link.imgURL}
+                alt={link.label}
+                width={16}
+                height={16}
+                className=
+                {`${isActive && 'invert-white'}`}
+                />
+                 <p className='tiny-medium'>{link.label}</p> 
+                </Link>
+            )
+          })}
+    </section>
   )
 }
 
