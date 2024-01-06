0c3aa97908b0f152a1d9a9963fbddc31f285d559
Mohammed <saadisaadi768@gmail.com>
2024-01-05 21:02:33 -0600

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
          <Route path="/explore" element={<Explore />} />
          <Route path="/saved" element={<Saved />} /> 
          <Route path="/all-users" element={<AllUsers />} />
-         <Route path="/create-post" element={<CreatePost />} />
+         <Route path="/create-post" element={<CreatePost />} /> 
          <Route path="/update-post/:id" element={<EditPost />} /> 
          <Route path="/posts/:id" element={<PostDetails />} /> 
          <Route path="/profile/:id/*" element={<Profile />} />
