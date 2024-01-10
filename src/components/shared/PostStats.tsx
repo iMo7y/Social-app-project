9da7eef4a183d9382374d6654d2d087be044bede
Mohammed <saadisaadi768@gmail.com>
2024-01-09 23:46:23 -0600

    changing icons

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,21 +1,19 @@
 import { Models } from "appwrite";
-import lottie from "lottie-web";
-import { defineElement } from "@lordicon/element";
-
-// define "lord-icon" custom element with default properties
-defineElement(lottie.loadAnimation);
+import animationData from "./Hart.json"
+import Lottie from "lottie-react"
 type PostStatsProps = {
     post: Models.Document;
     userId: string;
 }
-
 const PostStats = ({ post, userId }: PostStatsProps) => {
   return (
     <div className="flex justify-between items-center z-20">
-     <div className="flex gap-2 mr-5">
-      <img 
-        src="/assets/icons/like.svg"
-      />
+     <div className="flex gap-2 mr-5 w-20 h-20">
+        <Lottie 
+        className="cursor-pointer"
+        onClick={() => {}}
+        animationData={animationData}
+        />
      </div>
     </div>
   )
