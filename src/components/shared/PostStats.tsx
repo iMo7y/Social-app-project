459c175da6082c182ac01cfbe1eec1c37c8d211e
Mohammed <saadisaadi768@gmail.com>
2024-01-10 23:32:28 -0600

    working on like functionality

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,18 +1,57 @@
+import { useState, useEffect } from 'react';
+import { useUserContext } from "@/context/AuthContext";
+import { useDeleteSavedPost, useLikePost, useSavePost } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
-import animationData from "./Hart.json"
-import Lottie from "lottie-react"
+import { checkIsLiked } from '@/lib/utils';
+
 type PostStatsProps = {
     post: Models.Document;
     userId: string;
 }
 const PostStats = ({ post, userId }: PostStatsProps) => {
+  const likesList = post.likes.map((user: Models.Document) => user.$id)
+
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePost();
+  const { mutate: savePost } = useSavePost();
+  const { mutate: deleteSavedPost } = useDeleteSavedPost();
+
+  const { data: currnetUser } = useUserContext
+
+  const handleLikePost = () => {}
+
+  const handleSavePost = () => {}
+  
   return (
     <div className="flex justify-between items-center z-20">
-     <div className="flex gap-2 mr-5 w-20 h-20">
-        <Lottie 
+     <div className="flex gap-2 m4-5">
+        <img 
+        src={checkIsLiked(likes, userId) 
+        ? "/assets/icons/liked.svg"
+        : "/assets/icons/like.svg"}
+        
+        alt="like"
+        width={20}
+        height={20}
+        onClick={handleLikePost}
         className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">{likes.length}</p>
+     </div>
+
+     <div className="flex gap-2">
+        <img 
+        src={isSaved
+        ? "/assets/icons/saved.svg"
+        : "/assets/icons/save.svg"
+        }
+        alt="like"
+        width={20}
+        height={20}
         onClick={() => {}}
-        animationData={animationData}
+        className="cursor-pointer"
         />
      </div>
     </div>
